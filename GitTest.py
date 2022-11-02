9d2fe30e1a77d081a3d6fe4ed605c7c9a3eaec81
Jill Tran <jtran41205@gmail.com>
2022-11-01 20:25:28 -0400

    I have created a simple python file for you guys to edit for the assignment

--- GitTest.py
+++ GitTest.py
@@ -1,1 +1,12 @@
 import sys
+
+class shape:
+    """represents a shape
+
+    Attributes:
+        name (str): the name of the shape
+        sides(int): the number of sides this has
+    """
+
+
+print("Add whatever stuff. This is Jill's Edit")
