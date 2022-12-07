f155a1504a5bc62248dfb01c034279288a4353a7
Jill Tran <jtran41205@gmail.com>
2022-12-06 19:38:34 -0500

    First draft of Cipher class done

--- cipher_game.py
+++ cipher_game.py
@@ -1,7 +1,64 @@
 from argparse import ArgumentParser
+import re
 import sys
 # import whatever allows us to read user input
+POSITION = {
+    0: ["A","a"],
+    1: ["B","b"],
+    2: ["C","c"],
+    3: ["D","d"],
+    4: ["E","e"],
+    5: ["F","f"],
+    6: ["G","g"],
+    7: ["H","h"],
+    8: ["I","i"],
+    9: ["J","j"],
+    10: ["K","k"],
+    11: ["L","l"],
+    12: ["M","m"],
+    13: ["N","n"],
+    14: ["O","o"],
+    15: ["P","p"],
+    16: ["Q","q"],
+    17: ["R","r"],
+    18: ["S","s"],
+    19: ["T","t"],
+    20: ["U","u"],
+    21: ["V","v"],
+    22: ["W","w"],
+    23: ["X","x"],
+    24: ["Y","y"],
+    25: ["Z","z"]
+}
 
+ALPHABET = {
+    "A": 0, "a": 0,
+    "B": 1, "b": 1,
+    "C": 2, "c": 2,
+    "D": 3, "d": 3,
+    "E": 4, "e": 4,
+    "F": 5, "f": 5,
+    "G": 6, "g": 6,
+    "H": 7, "h": 7,
+    "I": 8, "i": 8,
+    "J": 9, "j": 9,
+    "K": 10, "k": 10,
+    "L": 11, "l": 11,
+    "M": 12, "m": 12,
+    "N": 13, "n": 13,
+    "O": 14, "o": 14,
+    "P": 15, "p": 15,
+    "Q": 16, "q": 16,
+    "R": 17, "r": 17,
+    "S": 18, "s": 18,
+    "T": 19, "t": 19,
+    "U": 20, "u": 20,
+    "V": 21, "v": 21,
+    "W": 22, "w": 22,
+    "X": 23, "x": 23,
+    "Y": 24, "y": 24,
+    "Z": 25, "z": 25
+}
 class Game:
     """
     Attributes: a list of Cipher objects. These Cipher objects will contain several strings detailing important parts of the game
@@ -19,8 +76,8 @@ class Game:
         Returns: none
         """
         with open(file,"r",encoding = "utf-8") as f:
-             for line in f:
-                 line.split("\t")
+            for line in f:
+                self.words.append(Cipher(line))
             
     def total_score():
         """This will take the average score from each of the Cipher objects and return that value
@@ -51,24 +108,39 @@ class Cipher:
     Attributes:
         answer (String): A string containing the original string without the "key=x", an encrypted version of the string
         encryption (String): the encrypted String
-        key (dict): this contains the direction and amount to shift the original string.
-        score (int)
+        key (int): this contains the amount to shift the original string.
+        score (int): displayed as a percentage
     """
     def __init__(self, line):
         """ Because each string is assumed to end with a "key=x" at the end, those will be separated at the beginning
             initialize answer based on that. Take 
         
-        Side effects: initializes the answer string and a dictionary object for the key to encrypt the string
+        Side effects: initializes the answer string and a int object for the key to encrypt the string
         
         runs the encrypt method to initialize the encrypted string
         """
-        pass
-    def encrypt(self, answer, key):
-        """ an encryption method that takes the key dictionary object and shifts the answer string
+        self.answer = re.search(r"(.+)Key=\b([1-9]|1[0-9]|2[0-6])\b", line).group(1)
+        self.key = re.search(r"(.+)Key=\b([1-9]|1[0-9]|2[0-6])\b", line).group(2)
+        self.encryption = ""
+        self.score = 0.0
         
-        Side effects: initializes the encryption string using the key.
+    def encrypt(self):
+        """ an encryption method that takes the key int  and shifts the answer string
+        
+        Side effects: initializes the encrypted string using the key.
         """
-        pass
+        case = 0
+        for character in range(0, len(self.answer)):
+            if self.answer.index(character).islower():
+                case = 1
+                
+            original = ALPHABET[self.answer.index(character)] 
+            shift = original - self.key
+            if self.key < 0:
+                shift += 26
+            
+            self.encryption += POSITION[shift][case]
+            
     def set_score(self, answer):
         """ Reads in the player's score and returns the percentage they got correct rounded down and displayed as an int
         
@@ -79,7 +151,16 @@ class Cipher:
         Returns:
             none
         """
- 
+        attempt = 0
+        total = len(self.answer)
+        if len(self.answer) != len(answer):
+            self.score = 0
+            print("incorrect length")
+            return self.score
+        for character in range(0, len(answer)):
+            if self.answer.lower().index(character) == answer.lower().index(character):
+                attempt += 1
+
 def main(file):
     """
     
